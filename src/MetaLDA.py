cd2416aa541846bed39fdb42659818fc8b515a82
Asim Unmesh <a.unmesh@gmail.com>
2018-04-27 06:00:12 +0530

    inference

--- src/MetaLDA.py
+++ src/MetaLDA.py
@@ -15,6 +15,7 @@ class MetaLDA:
 	'''
 	def __init__(self, mu, sigma_sq, beta, num_topics, vocab, lambda = None, pi = None):
 
+		#Store state variables in a dict.
 		self.mu = mu
 		self.sigma_sq = sigma_sq
 		self.beta = beta
@@ -32,6 +33,10 @@ class MetaLDA:
 		else:
 			self.pi = pi
 
+		self.state = {}
+		self.current_iteration = 0
+		self.current_state = {mu:}
+
 
 	'''
 	performs inference over an minibatch and updates posterior sample and state for self.lambda and self.pi.
@@ -43,7 +48,18 @@ class MetaLDA:
 	'''
 	def inference(self, minibatch):
 
-		#Sample z using Gibbs Sampling
-		#Infer Lambda
-		#Infer theta
+		#Sample z using Gibbs Sampling - needs Alpha, current z allocations for each word(to calculate ndk.), Current Theta
+		#Infer Lambda using SGLD - To infer Lambda need current Learning Rate , grad of log prior of lambda, grad of log likelihood of data, and a gaussian noise
+		#Infer theta using SGRLD - Needs A sample of large enough size to calculate expectation wrt to z. Needs beta , previous value of theta.
 		#Store the inferences in class variable
+
+		# As a minibatch comes in we need to calculate Alpha and initialize z for that mini-batch. A good initialisation will be important.
+		#For that sampling from Alpha will be required using mixing proportion.
+
+		#Once we have an initial z, we can calculate ndk./i and sample from posterior z.
+
+		#
+
+
+
+
